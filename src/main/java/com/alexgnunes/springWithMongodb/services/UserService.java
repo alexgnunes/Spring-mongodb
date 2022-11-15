c2e793f6f9ccff03d4fc000b6576f74491604718
Alex Nunes <alexgaribalde_tb@hotmail.com>
2022-11-15 08:01:05 -0300

    Created find by id

--- src/main/java/com/alexgnunes/springWithMongodb/services/UserService.java
+++ src/main/java/com/alexgnunes/springWithMongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.alexgnunes.springWithMongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alexgnunes.springWithMongodb.domain.User;
 import com.alexgnunes.springWithMongodb.repository.UserRepository;
+import com.alexgnunes.springWithMongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,9 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		} 
 }
