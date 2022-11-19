0a94ba2ffc615f3ce48a3999f1c1c7e45ddf245f
Alex Nunes <alexgaribalde_tb@hotmail.com>
2022-11-19 06:18:31 -0300

    Search using text, max date and min date

--- src/main/java/com/alexgnunes/springWithMongodb/repository/PostRepository.java
+++ src/main/java/com/alexgnunes/springWithMongodb/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.alexgnunes.springWithMongodb.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -9,10 +10,13 @@ import org.springframework.stereotype.Repository;
 import com.alexgnunes.springWithMongodb.domain.Post;
 
 @Repository
-public interface PostRepository extends MongoRepository<Post, String>{
+public interface PostRepository extends MongoRepository<Post, String>{	
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 	@Query("{ 'Body': { $regex: ?0, $options: 'i' } }")
 	List<Post> findByBody(String text);
 	
-	List<Post> findByTitleContainingIgnoreCase(String text);
+	@Query("{ $and: [ { date: { $gte: ?1 } }, { date: { $lte: ?2 } }, { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch (String text, Date minDate, Date maxDate);
 }
